4bf0b053961940b59d62c50ea58bcdd1912bb67f
aboc <abocd@qq.com>
2019-04-11 14:07:09 +0800

    修改文件名，修改调用方法

--- README.md
+++ README.md
@@ -5,15 +5,23 @@
 
 
 ### 启动方法
-http_run_exe.exe -exe="c:/a.exe|d:/b.exe" -port=8081
+http_run_exe.exe -exe="c:/a.exe|d:/b.exe" -name="程序名称|程序名称" -port=8081
 
 `多个程序使用|隔开，需要将\换成/`
 
 ### 命令调用方法
 
 `打开`
-http://localhost:8081/?status=open&exe=a.exe
+http://localhost:8081/controller/openapp?exe=a.exe
 
 
 `关闭`
-http://localhost:8081/?status=close&exe=a.exe
+http://localhost:8081/controller/closeapp?exe=a.exe
+
+
+`列表`
+http://localhost:8081/listapp
+
+
+`缩略图`
+http://localhost:8081/icoapp?exe=a.exe
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "os/exec"
+import (
+	"io/ioutil"
+	"os/exec"
+)
 import (
 	"encoding/json"
 	// "errors"
@@ -29,6 +32,9 @@ func runExe(exeAdress string) (err2 error, msg string) {
 }
 
 func closeExe(exeAdress string) (err2 error, msg string) {
+	if strings.Index(exeAdress, "video") == 0 {
+		exeAdress = "PotPlayerMini64.exe"
+	}
 	cmd := exec.Command("taskkill", "/f", "/t", "/im", exeAdress)
 	err := cmd.Run()
 	if err != nil {
@@ -40,17 +46,47 @@ func closeExe(exeAdress string) (err2 error, msg string) {
 	}
 }
 
+/**
+ *  列表程序
+ */
+func listExe() (err2 error, msg map[string]Exe) {
+	exeList := make(map[string]Exe)
+	for name, exe := range ExeList {
+		if exe.Show {
+			exeList[name] = exe
+		}
+	}
+	return nil, exeList
+}
+
 var port int
 var exe string
-var exeMaps map[string]string
+var name string
+
+type Exe struct {
+	Name string
+	Path string
+	Ico  string
+	Show bool
+}
+
+var ExeList map[string]Exe
 
 func main() {
-	exeMaps = make(map[string]string, 3)
+	var videoPlayer string
+	ExeList = make(map[string]Exe, 3)
 	//go run "d:\go\src\github.com\abocd\test\exec.go" -exe="D:/unity/xiaohu/build/xiaohu/xiaohu.exe|e:/xiaohu/xiaohu2.exe" -port=8081
 	flag.IntVar(&port, "port", 8080, "监听的端口号")
 	flag.StringVar(&exe, "exe", "D:/unity/xiaohu/build/xiaohu/xiaohu.exe", "监听的程序，多个用|隔开")
+	flag.StringVar(&name, "name", "语音精灵", "和程序配套的对应的程序名称，多个用|隔开")
+	flag.StringVar(&videoPlayer, "player", "PotPlayerMini64.exe", "视频播放器名称")
 	flag.Parse()
 	_exe := strings.Split(exe, "|")
+	_name := strings.Split(name, "|")
+	if len(_exe) != len(_name) {
+		fmt.Println("exe和name参数数量不一致")
+		return
+	}
 	for i := 0; i < len(_exe); i++ {
 		_exe[i] = path.Clean(_exe[i])
 		_, err := os.Stat(_exe[i])
@@ -58,45 +94,79 @@ func main() {
 			fmt.Println(err)
 			continue
 		}
-		exeMaps[path.Base(_exe[i])] = _exe[i]
+		var name = path.Base(_exe[i])
+		if i < len(_name) {
+			name = _name[i]
+		}
+		ExeList[path.Base(_exe[i])] = Exe{name, _exe[i], fmt.Sprintf("%s.jpg", _exe[i]), true}
 	}
-	fmt.Println(port, exeMaps)
+	ExeList[videoPlayer] = Exe{videoPlayer, videoPlayer, "", false}
+	fmt.Println(port, ExeList)
 	startServer()
 }
 
 func startServer() {
 	http.HandleFunc("/", web)
+	fmt.Println("服务器启动成功")
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		panic(err)
 	}
 }
 
+type resultMsgType interface{}
+
 type Msg struct {
-	Error   string
-	Success string
+	Error   resultMsgType
+	Success resultMsgType
+}
+
+func ico(exeAdress string, w http.ResponseWriter, r *http.Request) {
+	icoPath := ExeList[exeAdress].Ico
+	icoPath = path.Clean(icoPath)
+	_, err := os.Stat(icoPath)
+	fmt.Println("找图标 ", exeAdress, icoPath)
+	if err != nil {
+		return
+	}
+	data, err := ioutil.ReadFile(icoPath)
+	if err != nil {
+		return
+	}
+	w.Write(data)
 }
 
 func web(w http.ResponseWriter, r *http.Request) {
 	var msg Msg
-	fmt.Println(r.RequestURI)
+	fmt.Println(r.RequestURI, r.URL.Path)
 	status := r.URL.Query().Get("status")
-	exe := r.URL.Query().Get("exe")
-	fmt.Println(status, exe)
-	if _, ok := exeMaps[exe]; !ok {
-		fmt.Println(exe, "程序不存在")
-		msg.Error = fmt.Sprintf("%s程序不存在", exe)
-		jsonData, _ := json.Marshal(msg)
-		w.Write(jsonData)
-		return
+	// exe := r.URL.Query().Get("exe")
+	exe := r.PostFormValue("exe")
+	if exe == "" {
+		exe = r.URL.Query().Get("exe")
 	}
-	fmt.Println(exeMaps[exe])
+	fmt.Println(status, exe)
+	if r.URL.Path != "/listapp" {
+		if _, ok := ExeList[exe]; !ok {
+			fmt.Println(exe, "程序不存在")
+			msg.Error = fmt.Sprintf("%s程序不存在", exe)
+			jsonData, _ := json.Marshal(msg)
+			w.Write(jsonData)
+			return
+		}
+	}
+	fmt.Println(ExeList[exe])
 	var resultStatus error
-	var resultMsg string
-	if status == "open" {
-		resultStatus, resultMsg = runExe(exeMaps[exe])
-	} else {
+	var resultMsg resultMsgType
+	if r.URL.Path == "/controller/openapp" {
+		resultStatus, resultMsg = runExe(ExeList[exe].Path)
+	} else if r.URL.Path == "/controller/closeapp" {
 		resultStatus, resultMsg = closeExe(exe)
+	} else if r.URL.Path == "/icoapp" {
+		ico(exe, w, r)
+		return
+	} else {
+		resultStatus, resultMsg = listExe()
 	}
 	if resultStatus == nil {
 		msg.Success = resultMsg
