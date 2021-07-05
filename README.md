c9e19d8fc05fa766318164c1eb987b816eef0ae3
aboc <abocd@qq.com>
2021-07-05 17:51:57 +0800

    main

--- README.md
+++ README.md
@@ -5,23 +5,23 @@
 
 
 ### 启动方法
-http_run_exe.exe -exe="c:/a.exe|d:/b.exe" -name="程序名称|程序名称" -port=8081
+http_run_exe.exe -exe="c:/a.exe|d:/b.exe" -name="程序名称|程序名称" -port=8081  -single=1
 
 `多个程序使用|隔开，需要将\换成/`
 
 ### 命令调用方法
 
 `打开`
-http://localhost:8081/controller/openapp?exe=a.exe
+http://localhost:8081/open?exe=a.exe
 
 
 `关闭`
-http://localhost:8081/controller/closeapp?exe=a.exe
+http://localhost:8081/close?exe=a.exe
 
 
 `列表`
-http://localhost:8081/listapp
+http://localhost:8081/list
 
 
 `缩略图`
-http://localhost:8081/icoapp?exe=a.exe
+http://localhost:8081/ico?exe=a.exe
--- main.go
+++ main.go
@@ -1,35 +1,57 @@
 package main
 
 import (
-	"errors"
-	"io/ioutil"
-	"os/exec"
-	"runtime"
-)
-import (
 	"encoding/json"
-	// "errors"
+	"errors"
 	"flag"
 	"fmt"
-
-	// "log"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 	"path"
+	"runtime"
 	"strings"
+	"time"
 )
 
-func runExe(exeAdress string) (err2 error, msg string) {
+/**
+ *
+ * 正在运行的exe
+ **/
+var runingExes map[string]int64
+
+// "errors"
+
+// "log"
+
+func runExe(exe string) (err2 error, msg string) {
+	exeAdress := ExeList[exe].Path
 	dir := path.Dir(exeAdress)
 	os.Chdir(dir)
-	cmd := exec.Command("cmd.exe", "/c", "start "+exeAdress)
+	var cmd *exec.Cmd
+	if osType() == "windows" {
+		cmd = exec.Command("cmd.exe", "/c", "start "+exeAdress)
+	} else if osType() == "linux" {
+		cmd = exec.Command(exeAdress)
+	} else {
+		err := errors.New("不支持的系统")
+		fmt.Println("结束失败:", err)
+		return err, "结束失败，不支持的系统"
+	}
 
+	if single {
+		for name := range runingExes {
+			closeExe(name)
+		}
+	}
 	err := cmd.Run()
 	fmt.Println("正在启动程序", exeAdress)
 	if err != nil {
 		fmt.Println("启动失败:", err)
 		return err, "启动失败"
 	} else {
+		runingExes[exe] = time.Now().Unix()
 		fmt.Println("启动成功!")
 		return nil, "启动成功"
 	}
@@ -88,6 +110,7 @@ var port int
 var exe string
 var name string
 var videoPlayer string
+var single bool
 
 type Exe struct {
 	Name string
@@ -101,11 +124,13 @@ var ExeList map[string]Exe
 func main() {
 	fmt.Println(runtime.GOOS)
 	ExeList = make(map[string]Exe, 3)
+	runingExes = make(map[string]int64, 10)
 	//go run "d:\go\src\github.com\abocd\test\exec.go" -exe="D:/unity/xiaohu/build/xiaohu/xiaohu.exe|e:/xiaohu/xiaohu2.exe" -port=8081
 	flag.IntVar(&port, "port", 8081, "监听的端口号")
 	flag.StringVar(&exe, "exe", "D:/unity/xiaohu/build/xiaohu/xiaohu.exe", "监听的程序，多个用|隔开")
 	flag.StringVar(&name, "name", "语音精灵", "和程序配套的对应的程序名称，多个用|隔开")
 	flag.StringVar(&videoPlayer, "player", "PotPlayerMini64.exe", "视频播放器名称")
+	flag.BoolVar(&single, "single", false, "是否只允许运行单一程序")
 	flag.Parse()
 	_exe := strings.Split(exe, "|")
 	_name := strings.Split(name, "|")
@@ -172,7 +197,7 @@ func web(w http.ResponseWriter, r *http.Request) {
 		exe = r.URL.Query().Get("exe")
 	}
 	//fmt.Println(status, exe)
-	if r.URL.Path != "/listapp" {
+	if r.URL.Path != "/list" {
 		if _, ok := ExeList[exe]; !ok {
 			fmt.Println(exe, "程序不存在")
 			msg.Error = fmt.Sprintf("%s程序不存在", exe)
@@ -184,11 +209,11 @@ func web(w http.ResponseWriter, r *http.Request) {
 	//fmt.Println(ExeList[exe])
 	var resultStatus error
 	var resultMsg resultMsgType
-	if r.URL.Path == "/controller/openapp" {
-		resultStatus, resultMsg = runExe(ExeList[exe].Path)
-	} else if r.URL.Path == "/controller/closeapp" {
+	if r.URL.Path == "/open" {
+		resultStatus, resultMsg = runExe(exe)
+	} else if r.URL.Path == "/close" {
 		resultStatus, resultMsg = closeExe(exe)
-	} else if r.URL.Path == "/icoapp" {
+	} else if r.URL.Path == "/ico" {
 		ico(exe, w, r)
 		return
 	} else {
